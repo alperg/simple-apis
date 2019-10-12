d4f7f7fa55438336a0b6f0a95f3e0cc3512af29a
Alper Gokcehan <mail@alperg.com>
2019-10-12 13:40:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-var express = require('express');
-var cors = require('cors');
-var bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const PORT = process.env.PORT || 8080;
 
-var PORT = process.env.PORT || 8080;
-
-var app = express();
+const app = express();
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use('/', require('./api/customer'));
 app.use('/', require('./api/form'));
 app.use('/', require('./api/grocery'));
+app.use('/', require('./api/employee'));
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+  console.log(`App listening on PORT: ${PORT}`);
 });
